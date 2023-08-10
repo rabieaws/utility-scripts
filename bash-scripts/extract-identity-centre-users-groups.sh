426716a75c0af2c2bd0b7c5a94dbfdf018b6d283
rabieaws <119336964+rabieaws@users.noreply.github.com>
2023-08-10 14:49:26 -0400

    Update extract-identity-centre-users-groups.sh
    account for users who don't have any group memberships.

--- bash-scripts/extract-identity-centre-users-groups.sh
+++ bash-scripts/extract-identity-centre-users-groups.sh
@@ -14,12 +14,21 @@ aws identitystore list-users --identity-store-id "$identity_id" | jq -r '.Users[
     username=$(echo "$user_desc" | jq -r '.UserName')
     userdispalyname=$(echo "$user_desc" | jq -r '.DisplayName')
     useremail=$(echo "$user_desc" | jq -r '.Emails[].Value')
-    #For each user, list all the groups
-    aws identitystore list-group-memberships-for-member --identity-store-id "$identity_id" --member-id UserId="$userid" | jq -r '.GroupMemberships[].GroupId' | while read -r groupid; do
-        # Get group info
-        group_desc=$(aws identitystore describe-group --identity-store-id "$identity_id" --group-id "$groupid")
-        groupdispalyname=$(echo "$group_desc" | jq -r '.DisplayName')
-        # Write to the CSV file
-        echo "$username,$userdispalyname,$useremail,$groupdispalyname" >> output.csv 
-    done
+
+    # For each user, list all the groups
+    group_ids=$(aws identitystore list-group-memberships-for-member --identity-store-id "$identity_id" --member-id UserId="$userid" | jq -r '.GroupMemberships[].GroupId')
+
+    # Check if the user has any group memberships
+    if [[ -z "$group_ids" ]]; then
+        echo "$username,$userdispalyname,$useremail," >> output.csv
+    else
+        # If the user has group memberships, iterate over the groups and write to CSV
+        echo "$group_ids" | while read -r groupid; do
+            # Get group info
+            group_desc=$(aws identitystore describe-group --identity-store-id "$identity_id" --group-id "$groupid")
+            groupdispalyname=$(echo "$group_desc" | jq -r '.DisplayName')
+            # Write to the CSV file
+            echo "$username,$userdispalyname,$useremail,$groupdispalyname" >> output.csv 
+        done
+    fi
 done
