1fac4f4c58935cb0a0b08ad48d816e9d8c108671
rabieaws <119336964+rabieaws@users.noreply.github.com>
2023-08-11 08:09:04 -0400

    Update extract-identity-centre-users-groups.sh
    account for null values

--- bash-scripts/extract-identity-centre-users-groups.sh
+++ bash-scripts/extract-identity-centre-users-groups.sh
@@ -8,27 +8,28 @@ instance_id=$(aws sso-admin list-instances --output text --query 'Instances[0].I
 identity_id=$(aws sso-admin list-instances --output text --query 'Instances[0].IdentityStoreId')
 
 # List all users and iterate over them
-aws identitystore list-users --identity-store-id "$identity_id" | jq -r '.Users[].UserId' | while read -r userid; do
+users=$(aws identitystore list-users --identity-store-id "$identity_id" | jq -r '.Users? | select(.!=null)[] .UserId')
+echo "$users" | while read -r userid; do
     # Get needed user info
     user_desc=$(aws identitystore describe-user --identity-store-id "$identity_id" --user-id "$userid")
-    username=$(echo "$user_desc" | jq -r '.UserName')
-    userdispalyname=$(echo "$user_desc" | jq -r '.DisplayName')
-    useremail=$(echo "$user_desc" | jq -r '.Emails[].Value')
+    username=$(echo "$user_desc" | jq -r '.UserName? // ""')
+    userdisplayname=$(echo "$user_desc" | jq -r '.DisplayName? // ""')
+    useremail=$(echo "$user_desc" | jq -r '.Emails? | select(.!=null)[] .Value // ""')
 
     # For each user, list all the groups
-    group_ids=$(aws identitystore list-group-memberships-for-member --identity-store-id "$identity_id" --member-id UserId="$userid" | jq -r '.GroupMemberships[].GroupId')
+    group_ids=$(aws identitystore list-group-memberships-for-member --identity-store-id "$identity_id" --member-id UserId="$userid" | jq -r '.GroupMemberships? | select(.!=null)[] .GroupId')
 
     # Check if the user has any group memberships
     if [[ -z "$group_ids" ]]; then
-        echo "$username,$userdispalyname,$useremail," >> output.csv
+        echo "$username,$userdisplayname,$useremail," >> output.csv
     else
         # If the user has group memberships, iterate over the groups and write to CSV
         echo "$group_ids" | while read -r groupid; do
             # Get group info
             group_desc=$(aws identitystore describe-group --identity-store-id "$identity_id" --group-id "$groupid")
-            groupdispalyname=$(echo "$group_desc" | jq -r '.DisplayName')
+            groupdisplayname=$(echo "$group_desc" | jq -r '.DisplayName? // ""')
             # Write to the CSV file
-            echo "$username,$userdispalyname,$useremail,$groupdispalyname" >> output.csv 
+            echo "$username,$userdisplayname,$useremail,$groupdisplayname" >> output.csv 
         done
     fi
 done
